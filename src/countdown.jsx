bdc4345072b59c9578e001e41d8a1647083da4db
mohsen faraj <gmohsenfarajollahi@gmail.com>
2023-02-13 14:53:15 +0330

    adding outline and other improvements

--- src/countdown.jsx
+++ src/countdown.jsx
@@ -1,7 +1,7 @@
 function Countdown(props) {
     if (props.remaining < 0) {
         return (
-            <div>
+            <div className="countdownbox">
                 <h3>No Bus Found!</h3>
                 <p>there will be no bus after this time.</p>
             </div>
@@ -9,14 +9,15 @@ function Countdown(props) {
     }
     else if (props.remaining/3600 > 1) {
         return (
-            <div>
-                <h3>Remaining Time: More than one Hour!</h3>
+            <div className="countdownbox">
+                <h3>Remaining Time</h3>
+                <p>More than one Hour!</p>
             </div>
           );
     }
     else {
         return (
-            <div>
+            <div className="countdownbox">
                 <h3>Remaining Time</h3>
                     <p>{Math.floor(props.remaining/60)} Minutes and {props.remaining%60} Seconds</p>
             </div>
