511d13b037f6a680e7a72a6af06595bb43ddc1fe
mohsen faraj <gmohsenfarajollahi@gmail.com>
2023-07-14 23:57:47 +0330

    added autoscroll functionality

--- src/timer.jsx
+++ src/timer.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useRef } from "react";
+
 function Timer(props) {
+  const activebox = useRef();
+  const timebox = useRef();
+
+  useEffect(() => {
+    // scroll to active timer
+    if (activebox.current) {
+      timebox.current.scrollTo(0, activebox.current.offsetTop);
+    } else {
+      timebox.current.scrollTo(0, 0);
+    }
+  });
   const times = props.timer.times.map((time, index) => (
     <li key={time.toString()}>
-      <div className={index === props.timeIndex ? "active timebox" : "timebox"}>
+      <div
+        className={index === props.timeIndex ? "active timebox" : "timebox"}
+        ref={index === props.timeIndex ? activebox : null}
+      >
         <span>{time}</span>
         <span>10 Min</span>
       </div>
@@ -12,7 +28,7 @@ function Timer(props) {
       <h2>
         {props.timer.origin} - {props.timer.destiny}
       </h2>
-      <div className="timesContainer">
+      <div className="timesContainer" ref={timebox}>
         <ul>{times}</ul>
       </div>
     </>
