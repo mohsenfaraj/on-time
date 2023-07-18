2a79a67c0d222ab619e0d9c7558f2bdeccf1bf20
mohsen faraj <gmohsenfarajollahi@gmail.com>
2023-07-18 23:37:42 +0330

    testing workbox for caching

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({registerType: 'autoUpdate',
-    includeAssets: ['ontime192.png', 'ontime512.png'],
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,json,woff,woff2,ttf}"],
+      },
+      includeAssets: ["ontime192.png", "ontime512.png"],
       manifest: {
-        name: 'On-Time Bus time Tracker',
-        short_name: 'On-Time',
-        description: 'track the next bus arriving time with on time.',
-        theme_color: '#ffffff',
+        name: "On-Time Bus time Tracker",
+        short_name: "On-Time",
+        description: "track the next bus arriving time with on time.",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: 'ontime192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "ontime192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'ontime512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-  })
-  ] , 
-  base : '/on-time/'
-})
+            src: "ontime512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/on-time/",
+});
