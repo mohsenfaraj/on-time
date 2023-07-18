50d1ff5b66da6cde20cbe21ac3f2d8a90c8a13a8
mohsen faraj <gmohsenfarajollahi@gmail.com>
2023-07-18 23:50:55 +0330

    fix bug in vite.config

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,6 @@ export default defineConfig({
               },
             },
           },
-          ,
           {
             urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/.*/i,
             handler: "CacheFirst",
